836a4e63f803f8afeaa88b3967e5af586136387b
Glitch (hello-webpage) <none>
2020-07-07 14:03:22 +0000

    🎭🎩 Checkpoint ./script.js:18129410/3

--- script.js
+++ script.js
@@ -8,13 +8,13 @@
 
 function setup(){
   // Code here runs only once
-  createCanvas(800, 600)
+  createCanvas(800, 600);
 }
 
 function draw(){
   // Code here runs continuously
-  background(220)
+  background(220);
 
-  // ellipse(50, 50, 50, 50)
+  // ellipse(50, 50, 50, 50);
 
 }
