21cf9eae947ad531b64721f5ac41aae4f50572f5
Glitch (hello-webpage) <none>
2021-07-12 21:50:40 +0000

    🌃👫 Checkpoint ./script.js:33129225/811

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
- *    background, createCanvas, ellipse, noFill, stroke, strokeWeight, rect
+ *    background, createCanvas, ellipse, noFill, stroke, strokeWeight, rect, circle, fill, square, triangle
  */
 
 // Content behind double slashes is a comment. Use it for plain English notes,
@@ -14,16 +14,40 @@ function setup(){
 function draw(){
   // Code here runs continuously
   background(220);
-  stroke('rgb(0,255,0)');
-  fill('rgb(0,255,0)');
+  
+  //green square
+  stroke('rgb(0,220,0)');
+  fill('rgb(0,220,0)');
   square(30,30,100);
   
-  stroke('rgb(242,0,0)');
-  fill('rgb(242,0,0)');
+  //microsoft logo
+  stroke('rgb(246,83,20)');
+  fill('rgb(246,83,20)');
   square(160,30,45);
+  stroke('rgb(124,187,0)');
+  fill('rgb(124,187,0)');
   square(210,30,45);
+  stroke('rgb(0,161,241)');
+  fill('rgb(0,161,241)');
   square(160,80,45);
+  stroke('rgb(255,187,0)');
+  fill('rgb(255,187,0)');
   square(210,80,45);
+  
+  //target logo 
+  stroke('rgb(204,0,0)');
+  fill('rgb(204,0,0)');
+  circle(340,80,100);
+  stroke('rgb(220,220,220)');
+  fill('rgb(220,220,220)');
+  circle(340,80,67);
+  stroke('rgb(204,0,0)');
+  fill('rgb(204,0,0)');
+  circle(340,80,33);
+  
+  //mitsubishi logo
+  triangle(80,130,)
+  
   
   
   
