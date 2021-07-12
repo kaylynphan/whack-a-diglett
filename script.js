e544d2315365020144690421848a9cd8e7a58d8c
Glitch (hello-webpage) <none>
2021-07-12 19:52:32 +0000

    🌠🐉 Checkpoint ./script.js:33129225/77

--- script.js
+++ script.js
@@ -15,16 +15,18 @@ function draw(){
   // Code here runs continuously
   background(220);
 
-  ellipse(200, 100, 130);
+  strokeWeight(8);
+  
+  ellipse(100, 100, 80);
   
   noFill();
-  strokeWeight(8);
+
   stroke(60,123,219);
   
-  ellipse(100, 100, 130);
+  ellipse(200, 100, 80);
   
   strokeWeight(4);
   
-  ellipse(100, 150, 130);
+  
   
 }
