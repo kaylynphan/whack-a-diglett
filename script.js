7e7f04dfef7f280fda580db55acdd1cd28d368fa
Glitch (hello-webpage) <none>
2021-07-28 20:04:32 +0000

    🌄✍️ Checkpoint ./script.js:33129225/1178

--- script.js
+++ script.js
@@ -1,61 +1,16 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
- *    background, createCanvas, ellipse, noFill, stroke, strokeWeight, rect, circle, fill, square, triangle
+ *    createCanvas, colorMode, HSB, background
  */
 
 // Content behind double slashes is a comment. Use it for plain English notes,
 // or for code that you want to temporarily disable.
 
 function setup(){
-  // Code here runs only once
-  createCanvas(500, 300);
+  createCanvas(500,500);
+  colorMode(HSB,360,100,100);
 }
 
 function draw(){
-  // Code here runs continuously
-  background(220);
-  
-  //green square
-  stroke('rgb(0,220,0)');
-  fill('rgb(0,220,0)');
-  square(30,30,100);
-  
-  //microsoft logo
-  stroke('rgb(246,83,20)');
-  fill('rgb(246,83,20)');
-  square(160,30,45);
-  stroke('rgb(124,187,0)');
-  fill('rgb(124,187,0)');
-  square(210,30,45);
-  stroke('rgb(0,161,241)');
-  fill('rgb(0,161,241)');
-  square(160,80,45);
-  stroke('rgb(255,187,0)');
-  fill('rgb(255,187,0)');
-  square(210,80,45);
-  
-  //target logo 
-  stroke('rgb(204,0,0)');
-  fill('rgb(204,0,0)');
-  circle(340,80,100);
-  stroke('rgb(220,220,220)');
-  fill('rgb(220,220,220)');
-  circle(340,80,67);
-  stroke('rgb(204,0,0)');
-  fill('rgb(204,0,0)');
-  circle(340,80,33);
-  
-  //mitsubishi logo
-  triangle(80,160,30,247,130,247);
-  stroke('rgb(220,220,220)');
-  fill('rgb(220,220,220)');
-  triangle()
-  
-  
-  
-  
-  
-  
-  
-  
+  background(220, 0, 90);
 }
