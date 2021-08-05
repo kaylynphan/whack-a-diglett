64ca94b32d881ce3ff0ec781902d55028c4a708e
kaylynphan <82843288+kaylynphan@users.noreply.github.com>
2021-08-05 14:45:57 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 /*
   global
   createCanvas, soundFormats, width, height, random, loadImage, image, text, colorMode, HSB, textSize, textAlign, fill, LEFT, CENTER, background, 
-  createButton, mouseX, mouseY, keyCode, ENTER, mouseIsPressed, loadSound, io, setVolume,
+  createButton, mouseX, mouseY, keyCode, ENTER, mouseIsPressed, loadSound, io, setVolume, noLoop
 
 */
 
-let bgMusic, diglettSound, whack, ham, digletts, diglett1, diglett2, diglett3, diglett4, diglett5, pic, backgroundImage, positions, button, isStarted, backgroundColor, gameIsOver, score, ham2, lives; 
+let dugtrio1, dugtrio2, dugtrio3, dugtrio4, dugtrio5, bgMusic, diglettSound, whack, ham, digletts, diglett1, diglett2, diglett3, diglett4, diglett5, pic, backgroundImage, positions, button, isStarted, backgroundColor, gameIsOver, score, ham2, lives; 
 
 let teamScore = 0;
 
@@ -14,11 +14,13 @@ var socket;
 function preload() {
   soundFormats('mp3');
   whack = loadSound('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2Fhit.mp3?v=1628024180029');
-  whack.setVolume(0.004);
+  whack.setVolume(0.009);
   diglettSound = loadSound('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2Fdiglett.mp3?v=1628025327190');
-  diglettSound.setVolume(0.004);
-  bgMusic = loadSound('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2F5%20Minute%20Radial%20Timer%20-%20Pikachu%20Pokemon%20Edition!.mp3?v=1628107922085');
-  bgMusic.setVolume(0.004);
+  diglettSound.setVolume(0.009);
+  bgMusic = loadSound('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FbgMusic.mp3?v=1628109046494');
+  bgMusic.setVolume(0.001);
+  bgMusic.loop();
+
 }
 
 function setup(){
@@ -33,6 +35,11 @@ function setup(){
   diglett3 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0117.PNG?v=1627933218771');
   diglett4 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0118.PNG?v=1627933229166');
   diglett5 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0120.PNG?v=1627935502014');
+  dugtrio1 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0128%202.PNG?v=1628194448601');
+  dugtrio2 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0127.PNG?v=1628194451954');
+  dugtrio3 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0126.PNG?v=1628194456582');
+  dugtrio4 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0125.PNG?v=1628194460177');
+  dugtrio5 = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FIMG_0129.PNG?v=1628194462470');
   backgroundImage = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FScreen%20Shot%202021-08-02%20at%2012.41.38%20PM.png?v=1627933396644');
   pic = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2Fwhack-a-diglett.png?v=1627940291104');
   ham = loadImage('https://cdn.glitch.com/c130eace-6f5a-4c1f-9ec8-55327d44e593%2FHammer1.png?v=1628022610412');
@@ -42,11 +49,12 @@ function setup(){
   score = 0;
   lives = 20;
   
-  //needs filling in
-  digletts = [new Diglett(40, 90), new Diglett(200, 20), new Diglett(370, 100), new Diglett(620, 60), 
-              new Diglett(80, 240), new Diglett(230,180), new Diglett(500, 140), new Diglett(750, 210),
-              new Diglett(200, 360), new Diglett(400, 260), new Diglett(630, 270), new Diglett(380, 440),
-              new Diglett(440, 550), new Diglett(580, 490), new Diglett(690, 400)];
+  //1 is diglett, 3 is dugtrio
+  digletts = [new Diglett(40, 90, 1), new Diglett(200, 20, 3), new Diglett(370, 100, 1), new Diglett(620, 60, 1), 
+              new Diglett(80, 240, 1), new Diglett(230,180, 1), new Diglett(500, 140, 3), new Diglett(750, 210, 1),
+              new Diglett(200, 360, 1), new Diglett(400, 260, 1), new Diglett(630, 270, 1), new Diglett(380, 440, 1),
+              new Diglett(440, 550, 1), new Diglett(580, 490, 3), new Diglett(690, 400, 1)];
+  
 
 }
 
@@ -59,13 +67,11 @@ function draw(){
     start(); 
   } else if (isStarted) {
     gamePlay();  
-  }
-  else if (gameIsOver) {
+  } else if (gameIsOver) {
     gameOver();
   }
-  bgMusic.play();
+  //bgMusic.play();
   showHammer();
-  
 }
 
 function keyPressed() {
@@ -124,7 +130,6 @@ function gamePlay() {
   isStarted = true;
   let sound = random(400);
       if(sound < 1){
-        //diglettSound.setVolume(0.3);
         diglettSound.play();
       }
   showScores();
@@ -138,15 +143,17 @@ function gamePlay() {
 function gameOver() {
   lives = 0;
   gameIsOver = true;
+  background(backgroundColor);
   textSize(25);
   fill('black');
   textAlign(CENTER);
   text('Game Over', width / 2, height / 1.8);
+  noLoop();
   
 }
 
 class Diglett {
-  constructor(x, y){
+  constructor(x, y, pointValue){
     this.x = x;
     this.y = y;
     //the click area is a rectangle with the upper left corner as (x,y) and a width and height
@@ -155,9 +162,7 @@ class Diglett {
     this.time = 0;
     this.active = false; // True if the diglett is moving
     this.whacked = false; // True if the diglett has been whacked
-    //this.soundEffect = ;
-    this.pointValue = 1;
-    //this.status = 1;
+    this.pointValue = pointValue;
   }
   
   move() {
@@ -168,6 +173,16 @@ class Diglett {
   }
   
   display() {
+    if(!gameIsOver){
+      if (this.pointValue == 1) {
+        this.displayDiglett();
+      } else {
+        this.displayDugtrio();
+      }
+    }
+  }
+  
+  displayDiglett() {
     if (this.time == 0) {
       //inactive
       image(diglett1, this.x, this.y, this.width, this.height);
@@ -200,18 +215,50 @@ class Diglett {
     }
   }
   
+  displayDugtrio(){
+    if (this.time == 0) {
+      //inactive
+      image(dugtrio1, this.x, this.y, this.width, this.height);
+    } else if (this.time <= 5) {
+      image(dugtrio2, this.x, this.y, this.width, this.height);
+    } else if (this.time <= 10) {
+      if (this.whacked) {
+        image(dugtrio5, this.x, this.y, this.width, this.height);
+      } else {
+        image(dugtrio3, this.x, this.y, this.width, this.height);
+      }
+    } else if (this.time <= 90) {
+      if (this.whacked) {
+        image(dugtrio5, this.x, this.y, this.width, this.height);
+      } else {
+        image(dugtrio4, this.x, this.y, this.width, this.height);
+      }
+    } else if (this.time <= 95) {
+      image(dugtrio3, this.x, this.y, this.width, this.height);
+    } else if (this.time < 100) {
+      image(dugtrio2, this.x, this.y, this.width, this.height);
+    } else {
+      //reset dugtrio, or if no lives left end game
+      if (!this.whacked) {
+        lives > 1 ? lives-- : gameOver();
+      }
+      this.time = 0;
+      this.active = false;
+      this.whacked = false;
+    }
+  }
+  
   whack() {
     if (this.time > 5 && this.whacked == false) {
       this.whacked = true;
       //score point
       score += this.pointValue; 
       teamScore += this.pointValue;
+      whack.play();
       var data = {
         teamSc: teamScore
       }
       socket.emit('team score', data);
-      //whack.setVolume(0.0);
-      whack.play();
     }
   }
   
